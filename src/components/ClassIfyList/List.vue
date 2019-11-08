46a145516e6b61291c68c3c551cfcb7088348e0f
LeiTing2020 <1397379614@qq.com>
2019-11-08 14:45:31 +0800

    lt

--- src/components/ClassIfyList/List.vue
+++ src/components/ClassIfyList/List.vue
@@ -1,20 +1,21 @@
 <template>
-
-<div>
-  <Head></Head>
-  <handle-list></handle-list>
-</div>
+  <div>
+    <Head></Head>
+    <handle-list></handle-list>
+  </div>
 </template>
 <script>
 import Head from "./Header";
-import HandleList from './HandleList';
+import HandleList from "./HandleList";
+
+
 export default {
   components: {
     Head,
     HandleList
   },
-}
 
+};
 </script>
 <style lang="less" scoped>
 </style>
