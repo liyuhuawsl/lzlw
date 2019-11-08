a865bceb0050ef808fb434e1f736438facfaed8f
weihejiao <996764313@qq.com>
2019-11-08 14:09:43 +0800

    提交分支

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import store from './store/index';
 /* 导入公共样式 */
 import './assets/reset.min.css';
 import './assets/font/iconfont.css';
+import './assets/w-font/font/iconfont.css';
 import ElementUI from 'element-ui';
 import "element-ui/lib/theme-chalk/index.css";
 import Swiper from 'vue-awesome-swiper';
